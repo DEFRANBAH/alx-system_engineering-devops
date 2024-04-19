78a786daa77d2659b6343fc0ae83b9b1206ee8db
kelvin defrNBAH <kevinocholah@gmail.com>
2024-04-20 01:26:56 +0300

    puppet config in /tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# using Puppet to create a file in /tmp	
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "I love Puppet\n",
+  }
